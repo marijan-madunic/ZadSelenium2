aabba0357aa56c63738e9067b64e41687a2a71d8
marijan-madunic <32534705+marijan-madunic@users.noreply.github.com>
2020-08-31 10:18:13 +0200

    updated tests due to changes

--- ZadSelenium2/src/testing2/NewTestNGClass2.java
+++ ZadSelenium2/src/testing2/NewTestNGClass2.java
@@ -1,20 +1,12 @@
 package testing2;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.aventstack.extentreports.Status;
 
 
 public class NewTestNGClass2 extends BaseClass2 {	
 	
-	private Object[] selenuim_commands;
 
 	@Test
 	public void OpenSite() throws Exception {
@@ -23,53 +15,29 @@ public class NewTestNGClass2 extends BaseClass2 {
 		
 		driver.get("https://demoqa.com/automation-practice-form/");
 		
-		Assert.assertEquals(driver.findElement(By.cssSelector("strong")).getText(), "Read this message in your script and Print this in Console");
-		System.out.println("ToolsQA is a leading provider of technology content for learning.");
+
+		driver.findElement(By.id("firstName")).sendKeys("Pero");
+		driver.findElement(By.id("lastName")).sendKeys("Peric");
+		driver.findElement(By.id("userEmail")).sendKeys("Pero@example.com");
+			
 		
-		//driver.findElement(By.id("menu-item-374")).click();
-		//jse.executeScript("scroll(250, 0)");
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[2]/div[2]/div/form/fieldset/div[8]/input")).sendKeys("Pero");
+		driver.findElements(By.xpath("/html/body/div[1]/div/div/div[2]/div[2]/div[1]/form/div[3]/div[2]/div[1]/label")).get(0).click();
 		
-		driver.findElement(By.id("lastname")).sendKeys("Peric");
+		driver.findElement(By.id("userNumber")).sendKeys("0951234567");		
 		
-		//driver.findElements(By.name("radio_4[]")).get(0).click();
-		//driver.findElements(By.name("checkbox_5[]")).get(1).click();
-		
-		driver.findElement(By.id("sex-0")).click();
-		driver.findElement(By.id("exp-5")).click();
-		
-		driver.findElement(By.id("datepicker")).sendKeys("06.04.2020.");
-		
-		driver.findElement(By.id("profession-0")).click();
-		driver.findElement(By.id("profession-1")).click();
-		
-		driver.findElement(By.id("photo")).sendKeys(System.getProperty("user.dir")+".\\Photo\\image_photo.jpg");
-		
-		Select drpContinents = new Select(driver.findElement(By.name("continents")));
-		drpContinents.selectByValue("EU");
-				
-		Select selenium_commands = new Select(driver.findElement(By.id("selenium_commands")));
-		selenium_commands.selectByVisibleText("Browser Commands");
-		selenium_commands.selectByVisibleText("Switch Commands");
+		driver.findElement(By.id("dateOfBirthInput")).click();
+		driver.findElements(By.xpath("/html/body/div[1]/div/div/div[2]/div[2]/div[1]/form/div[5]/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[4]/div[3]")).get(0).click();
 		
 		
-		//Assert.assertEquals(driver.findElement(By.cssSelector("h1.entry-title")).getText(), "Datepicker");
+		driver.findElements(By.xpath("/html/body/div[1]/div/div/div[2]/div[2]/div[1]/form/div[7]/div[2]/div[1]/label")).get(0).click();
 		
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/aside[2]/ul/li[18]/a")).click();
-		driver.findElement(By.id("datepicker")).sendKeys("03/06/2020");
+		driver.findElement(By.id("uploadPicture")).sendKeys(System.getProperty("user.dir")+".\\Photo\\image_photo.jpg");
 		
-		driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/aside[1]/ul/li[4]/a")).click();
-		Actions act = new Actions(driver);
-		WebElement source = driver.findElement(By.id("draggable")); 
-		WebElement target = driver.findElement(By.id("droppable")); 
-		act.dragAndDrop(source,target).perform();
+		driver.findElement(By.id("currentAddress-label")).click();
+		driver.findElement(By.id("currentAddress")).sendKeys("Street");
+		
 		
 		testInfo.log(Status.INFO, "This is an example of test");
 		
-		/*
-		driver.findElement(By.id("description")).sendKeys("Hi, I am Pero");
-		driver.findElement(By.id("password_2")).sendKeys("PeroPericPerica");
-		driver.findElement(By.id("confirm_password_password_2")).sendKeys("PeroPericPerica");
-		*/
 	}
 }
